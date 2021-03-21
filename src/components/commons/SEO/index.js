e21c016af0561b16c8748dd76cc22ea050233d98
adrielerodr <adriele.rodrigues@accenture.com>
2021-03-21 18:48:56 -0300

    feat: módulo 3 - Aula 3

---
+++ src/components/commons/SEO/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+function SEO({ headTitle }) {
+  const pageTitleDefault = 'Instalura - Projeto do Alura JAMStack Bootcamp';
+  const pageUrl = 'https://metatags.io/';
+  const pageImage = 'https://www.alura.com.br/assets/img/alura-share.1571848411.png';
+  const pageDescription = 'With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!';
+
+  const hasHeadTitle = Boolean(headTitle);
+  const title = hasHeadTitle
+    ? (`${headTitle} | ${pageTitleDefault}`)
+    : (pageTitleDefault);
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={pageDescription} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={pageUrl} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={pageDescription} />
+      <meta property="og:image" content={pageImage} />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={pageUrl} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={pageDescription} />
+      <meta property="twitter:image" content={pageImage} />
+    </Head>
+  );
+}
+
+export default SEO;
+
+SEO.defaultProps = {
+  headTitle: '',
+};
+
+SEO.propTypes = {
+  headTitle: PropTypes.string,
+};
