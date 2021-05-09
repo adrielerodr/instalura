fcae5072019a75d899203897c84d6fa109add08f
adrielerodr <adriele.rodrigues@accenture.com>
2021-05-09 12:10:57 -0300

    feat(about page): add GraphQl

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function AboutPage() {
-  return (<div> Página Sobre </div>);
+export async function getStaticProps() {
+  const TOKEN = '3dd909849811b30c95ed972415b7e0';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageAbout {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
