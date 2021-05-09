b06bff5653141e927a55123d062ba670f3116f07
adrielerodr <adriele.rodrigues@accenture.com>
2021-05-09 12:32:29 -0300

    refactor(about page): create CSMGraphQlClient

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,9 @@
 import { GraphQLClient, gql } from 'graphql-request';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
 export async function getStaticProps() {
-  const TOKEN = '3dd909849811b30c95ed972415b7e0';
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-
-  const query = gql`
-    query {
-      pageAbout {
-        pageTitle
-        pageDescription
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
+  const messages = await getContent();
 
   return {
     props: {
